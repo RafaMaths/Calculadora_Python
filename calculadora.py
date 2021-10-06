90dfeba0dd97f7afb9403d94b30089483d63c31c
Rafael Mathias <rmathscoach@gmail.com>
2021-10-05 23:09:35 -0300

    criação de tamanho de janela

--- calculadora.py
+++ calculadora.py
@@ -4,7 +4,16 @@ from tkinter import ttk
 
 # declara janela
 window = Tk()
-window.title = "Calculadora"
+window.title("Calculadora")
+
+# definicao de configurações de largura e comprimento
+window.geometry("235x318")
+
+# definicao de duas janelas: display com cálculo e botões
+frame_window = Frame(window, width="235", height="50")
+frame_window.grid(row=0, column=0)
+
+
 
 # executa eventos da janela
 window.mainloop()
