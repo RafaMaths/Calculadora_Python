a2b1295dac371f73314aef919735cdade500ef75
Rafael Mathias <rmathscoach@gmail.com>
2021-10-06 08:56:09 -0300

    Crio cores de background e frame. Altero o tamanho do window

--- calculadora.py
+++ calculadora.py
@@ -2,16 +2,22 @@
 from tkinter import *
 from tkinter import ttk
 
+#cores
+
+backgroundColor = "#0d0901"
+frameColor = "#524f49"
+
 # declara janela
 window = Tk()
 window.title("Calculadora")
 
 # definicao de configurações de largura e comprimento
-window.geometry("235x318")
+window.geometry("400x500")
+window.config(bg = backgroundColor)
 
 # definicao de duas janelas: display com cálculo e botões
-frame_window = Frame(window, width="235", height="50")
-frame_window.grid(row=0, column=0)
+frame_window = Frame(window, width = "400", height = "100", bg = frameColor)
+frame_window.grid(row = 0, column = 0)
 
 
 
