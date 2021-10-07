19d95b0b6d1a19eb67eb0f66ff4326369c394a1d
Rafael Mathias <rmathscoach@gmail.com>
2021-10-07 12:59:20 -0300

    adiciono cores de paleta e crio os botões da primeira linha

--- calculadora.py
+++ calculadora.py
@@ -5,9 +5,10 @@ from tkinter import ttk
 #cores
 
 backgroundColor = "#0d0901"
-frameColor = "#524f49"
-operationButton = "#f27130"
-numberBotton = "#ccc8c6"
+frameHeadColor = "#524f49"
+operationButtonColor = "#f27130"
+numberBottonColor = "#ccc8c6"
+modelColor = "#3054f2"
 
 # declara janela
 window = Tk()
@@ -18,12 +19,22 @@ window.geometry("400x500")
 window.config(bg = backgroundColor)
 
 # frame de calculo
-frameHead = Frame(window, width = "400", height = "100")
+frameHead = Frame(window, width = "400", height = "100", bg = frameHeadColor)
 frameHead.grid(row = 0, column = 0)
 
 # frame de botões
 frameBody = Frame(window, width = "400", height = "400")
-frameBody.grid(row = 1, column = 0) 
+frameBody.grid(row = 1, column = 0)
+
+#cria botões
+clean = Button(frameBody, text = "C", width = 26, height= 4)
+clean.place(x = 0, y = 0)
+
+module = Button(frameBody, text = "%", width = 13, height = 4)
+module.place(x = 195, y = 0)
+
+division = Button(frameBody, text = "/", width = 13, height = 4)
+division.place(x = 300, y = 0)
 
 # executa eventos da janela
 window.mainloop()
