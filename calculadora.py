08cb4239124ac3d0cac20901f0cd6da1c5f2a0fb
Rafael Mathias <rmathscoach@gmail.com>
2021-10-08 01:05:16 -0300

    crio a função para receber os valores para apresentação em tela e altero os botões para receber a função

--- calculadora.py
+++ calculadora.py
@@ -26,8 +26,22 @@ frameHead.grid(row = 0, column = 0)
 frameBody = Frame(window, width = "235", height = "268")
 frameBody.grid(row = 1, column = 0)
 
+# variável para receber valores digitados
+everyValues = ''
+
+#recebe valores digitados
+def inputValues(value):
+
+    # declaro como global para que possa ser acessada pela variável externa(global)
+    global everyValues
+    everyValues += str(value)
+    
+    #passando valor para tela
+    resultText.set(everyValues)
+
 #criação de labels
-appLabel = Label(frameHead, text = '123456789', font = "Arial, 16", bg = brown, fg = white, width = 19, height = 2, padx = 5, pady = 5, relief = FLAT, anchor="e", justify=RIGHT)
+resultText = StringVar()
+appLabel = Label(frameHead, textvariable = resultText, font = "Arial, 16", bg = brown, fg = white, width = 19, height = 2, padx = 5, pady = 5, relief = FLAT, anchor="e", justify=RIGHT)
 appLabel.grid(row = 0, column = 0)
 
 #cria botões
@@ -35,59 +49,59 @@ appLabel.grid(row = 0, column = 0)
 clean = Button(frameBody, text = "C", width = 11, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 clean.place(x = 0, y = 0)
 
-module = Button(frameBody, text = "%", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+module = Button(frameBody, command = lambda: inputValues('%'), text = "%", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 module.place(x = 118, y = 0)
 
-division = Button(frameBody, text = "/", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+division = Button(frameBody, command = lambda: inputValues('%'), text = "/", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 division.place(x = 177, y = 0)
 
 #line2
-seven = Button(frameBody, text = "7", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+seven = Button(frameBody, command = lambda: inputValues('7'),  text = "7", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 seven.place(x = 0, y = 52)
 
-eight = Button(frameBody, text = "8", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+eight = Button(frameBody, command = lambda: inputValues('8'), text = "8", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 eight.place(x = 60, y = 52)
 
-nine = Button(frameBody, text = "9", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+nine = Button(frameBody, command = lambda: inputValues('9'), text = "9", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 nine.place(x = 118, y = 52)
 
-mult = Button(frameBody, text = "X", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+mult = Button(frameBody, command = lambda: inputValues('7'), text = "X", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 mult.place(x = 177, y = 52)
 
 #line3
-six = Button(frameBody, text = "6", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+six = Button(frameBody, command = lambda: inputValues('6'), text = "6", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 six.place(x = 0, y = 104)
 
-five = Button(frameBody, text = "5", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+five = Button(frameBody, command = lambda: inputValues('5'), text = "5", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 five.place(x = 60, y = 104)
 
-four = Button(frameBody, text = "4", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+four = Button(frameBody, command = lambda: inputValues('4'), text = "4", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 four.place(x = 118, y = 104)
 
-subt = Button(frameBody, text = "-", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+subt = Button(frameBody, command = lambda: inputValues('-'), text = "-", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 subt.place(x = 177, y = 104)
 
 #line4
-three = Button(frameBody, text = "3", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+three = Button(frameBody, command = lambda: inputValues('3'), text = "3", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 three.place(x = 0, y = 156)
 
-two = Button(frameBody, text = "2", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+two = Button(frameBody, command = lambda: inputValues('2'), text = "2", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 two.place(x = 60, y = 156)
 
-one = Button(frameBody, text = "1", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+one = Button(frameBody, command = lambda: inputValues('1'), text = "1", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 one.place(x = 118, y = 156)
 
-sum = Button(frameBody, text = "+", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+sum = Button(frameBody, command = lambda: inputValues('+'), text = "+", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 sum.place(x = 177, y = 156)
 
 #line5
-naught = Button(frameBody, text = "C", width = 11, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+naught = Button(frameBody, command = lambda: inputValues('0'), text = "0", width = 11, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 naught.place(x = 0, y = 208)
 
-dot = Button(frameBody, text = ".", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+dot = Button(frameBody, command = lambda: inputValues('.'), text = ".", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 dot.place(x = 118, y = 208)
 
-equal = Button(frameBody, text = "=", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+equal = Button(frameBody, command = lambda: inputValues('%'), text = "=", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 equal.place(x = 177, y = 208)
 
 # executa eventos da janela
