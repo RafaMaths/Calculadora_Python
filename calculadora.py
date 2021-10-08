6679108c12156df26f3f6e96f7a8505e7247132e
Rafael Mathias <rmathscoach@gmail.com>
2021-10-08 00:36:34 -0300

    crio a quarta e a quinta linha de botões e faço a configuração da label onde se apresentam os números de resultado

--- calculadora.py
+++ calculadora.py
@@ -15,16 +15,20 @@ window = Tk()
 window.title("Calculadora")
 
 # definicao de configurações de largura e comprimento
-window.geometry("235x318")
+window.geometry("235x314")
 window.config(bg = black)
 
 # frame de calculo
-frameHead = Frame(window, width = "235", height = "50", bg = brown)
+frameHead = Frame(window, width = "235", height = "58", bg = brown)
 frameHead.grid(row = 0, column = 0)
 
 # frame de botões
 frameBody = Frame(window, width = "235", height = "268")
 frameBody.grid(row = 1, column = 0)
+
+#criação de labels
+appLabel = Label(frameHead, text = '123456789', font = "Arial, 16", bg = brown, fg = white, width = 19, height = 2, padx = 5, pady = 5, relief = FLAT, anchor="e", justify=RIGHT)
+appLabel.grid(row = 0, column = 0)
 
 #cria botões
 #line1
@@ -34,7 +38,7 @@ clean.place(x = 0, y = 0)
 module = Button(frameBody, text = "%", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 module.place(x = 118, y = 0)
 
-division = Button(frameBody, text = "/", width = 5, height = 2, bg = orange, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+division = Button(frameBody, text = "/", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 division.place(x = 177, y = 0)
 
 #line2
@@ -47,7 +51,7 @@ eight.place(x = 60, y = 52)
 nine = Button(frameBody, text = "9", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 nine.place(x = 118, y = 52)
 
-mult = Button(frameBody, text = "X", width = 5, height = 2, bg = orange, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+mult = Button(frameBody, text = "X", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 mult.place(x = 177, y = 52)
 
 #line3
@@ -60,11 +64,31 @@ five.place(x = 60, y = 104)
 four = Button(frameBody, text = "4", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 four.place(x = 118, y = 104)
 
-subt = Button(frameBody, text = "-", width = 5, height = 2, bg = orange, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+subt = Button(frameBody, text = "-", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
 subt.place(x = 177, y = 104)
 
+#line4
+three = Button(frameBody, text = "3", width = 5, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+three.place(x = 0, y = 156)
 
+two = Button(frameBody, text = "2", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+two.place(x = 60, y = 156)
 
+one = Button(frameBody, text = "1", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+one.place(x = 118, y = 156)
+
+sum = Button(frameBody, text = "+", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+sum.place(x = 177, y = 156)
+
+#line5
+naught = Button(frameBody, text = "C", width = 11, height= 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+naught.place(x = 0, y = 208)
+
+dot = Button(frameBody, text = ".", width = 5, height = 2, bg = gray, fg = black, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+dot.place(x = 118, y = 208)
+
+equal = Button(frameBody, text = "=", width = 5, height = 2, bg = orange, fg = white, font = ('Ivy 13 bold'), relief = RAISED, overrelief = RIDGE)
+equal.place(x = 177, y = 208)
 
 # executa eventos da janela
 window.mainloop()
